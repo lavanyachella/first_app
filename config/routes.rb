1d5372f2f979437f08148f426c650f9d421f097e
Lavanya Chella <lavanyachella.me@gmail.com>
2012-12-31 22:53:07 -0800

    Finish demo app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 FirstApp::Application.routes.draw do
+  resources :micro_posts
+
+
+  resources :users
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
